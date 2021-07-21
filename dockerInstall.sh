a1472e5c7ebff389f791f9b691ba311ba0add547
zarrond <47771154+zarrond@users.noreply.github.com>
2021-07-21 13:24:48 +0300

    Add files via upload

---
+++ dockerInstall.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade
+sudo apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update && apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -aG docker $(whoami)
+sudo systemctl status docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
